ff59f7e52defc760a0f1ceb80d86ec9118228482
Serhii Shabala <sergei181989@gmail.com>
2023-02-13 21:56:34 +0200

    :recycle: Clean & refactoring code in the project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-import { Aside, Main } from './components/';
-
 export const App = () => {
-  return (
-    <div className='App'>
-      <Aside />
-      <Main />
-    </div>
-  );
+  return <div className='App'>App Todo</div>;
 };
--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export * from './AddTask/';
-export * from './Aside/';
-export * from './Main/';
-export * from './Tasks/';
-export * from './TodoList/';
